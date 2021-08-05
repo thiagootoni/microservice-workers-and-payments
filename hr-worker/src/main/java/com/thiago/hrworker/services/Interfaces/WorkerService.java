53722b385036e003742de95ed94da11421e53f2b
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-08-04 21:20:24 -0300

    ajustes

--- hr-worker/src/main/java/com/thiago/hrworker/services/Interfaces/WorkerService.java
+++ hr-worker/src/main/java/com/thiago/hrworker/services/Interfaces/WorkerService.java
@@ -1,5 +1,7 @@
 package com.thiago.hrworker.services.Interfaces;
 
 public interface WorkerService {
+	
+	
 
 }
--- hr-worker/src/main/java/com/thiago/hrworker/services/WorkerServiceImpl.java
+++ hr-worker/src/main/java/com/thiago/hrworker/services/WorkerServiceImpl.java
@@ -1,5 +1,8 @@
 package com.thiago.hrworker.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class WorkerServiceImpl {
 
 }
