7bda566c50e2b9cecf5a79f8a0d778f837d45822
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-08-20 20:41:10 -0300

    Configurando Eureka server e Eureka clients

--- hr-worker/src/main/java/com/thiago/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/thiago/hrworker/HrWorkerApplication.java
@@ -3,7 +3,9 @@ package com.thiago.hrworker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication implements CommandLineRunner  {
 
