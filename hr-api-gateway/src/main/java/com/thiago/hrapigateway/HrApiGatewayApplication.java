8326a0992f2fcd450f76df89b7e4b8546f13d70a
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-08-26 20:50:05 -0300

    Serviço de Api Gateway Criado
    Criado serviço de api gateway com Spring cloud gateway
    Configurado o ambiente pro Eureka
    Configurados os caminhos para o acesso aos microsserviços de workers e payments

---
+++ hr-api-gateway/src/main/java/com/thiago/hrapigateway/HrApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.thiago.hrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayApplication.class, args);
+	}
+
+}
